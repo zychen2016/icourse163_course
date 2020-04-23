d98b2158eb09cd108f82a1a47d7acde67a0b56be
zychen2016 <zeyuchen2016@gmail.com>
2020-04-23 15:46:55 +0800

    -
    -

--- 程序设计入门C语言/test.c
+++ 程序设计入门C语言/test.c
@@ -2,12 +2,13 @@
 
 int main()
 {
+	const int AMOUNT=100;
     int price = 0;
 
     printf("请输入金额（元）：");
     scanf("%d", &price);
 
-    int change = 100 - price;
+    int change = AMOUNT- price;
 
     printf("找您%d元。\n", change);
 
