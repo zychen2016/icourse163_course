c07a31e8bdf991d4d9eaaf644ebab871fec3fc70
zychen2016 <zeyuchen2016@gmail.com>
2020-05-01 08:16:32 +0800

    update
    update

---
+++ 程序设计入门C语言/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	char c;
+	int i;
+	scanf("%d",&i);
+	c=i;
+	printf("c=%d\n", c);
+	printf("c='%c'\n", c);
+    return 0;
+}
---
+++ 程序设计入门C语言/swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int a=5;
+	int b=6;
+	int t;
+	t=a;
+	a=b;
+	b=t;
+	printf("a=%d,b=%d",a,b);
+	return 0;
+}
+
